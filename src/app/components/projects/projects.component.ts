c9d38cf451ffcbd5775466f98dd7aecfc76facae
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-10-21 18:14:49 +0100

    Created Cards to show projects

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class ProjectsComponent {
 
+  projects: {projectName: string, projectDescription:string, projectSourceURL: string, projectHostURL: string, previewImgPath: string}[] = [
+    {
+      projectName: 'Web Project List',
+      projectDescription: 'Application built using React and Express that allows a user to interact with a custom API to create and manage a list of web applications',
+      projectSourceURL: 'https://github.com/Grattade07/Web-Project-List',
+      projectHostURL: 'https://web-project-list.onrender.com/',
+      previewImgPath: '/assets/project-preview-imgs/Web-Project-List-Preview.png'
+    },
+    {
+      projectName: 'Web Project List',
+      projectDescription: 'Application built using React and Express that allows a user to interact with a custom API to create and manage a list of web applications',
+      projectSourceURL: 'https://github.com/Grattade07/Web-Project-List',
+      projectHostURL: 'https://web-project-list.onrender.com/',
+      previewImgPath: '/assets/project-preview-imgs/Web-Project-List-Preview.png'
+    },
+  ]
 }
