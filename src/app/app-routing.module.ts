0f8a210bfcfe247402695793461a314a865956f8
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-10-21 23:47:31 +0100

    Contact page - Created contact page and linked form to external site for submit management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './components/about/about.component';
 import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'about'},
   {path: 'about', component: AboutComponent, data: {state: 'about'}},
   {path: 'projects', component: ProjectsComponent, data: {state: 'projects'}},
+  {path: 'contact', component: ContactPageComponent, data: {state: 'contact'}},
   {path:'**', redirectTo: 'about', pathMatch: 'full'}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from './components/about/about.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { ProjectsComponent } from './components/projects/projects.component'
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     AboutComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
