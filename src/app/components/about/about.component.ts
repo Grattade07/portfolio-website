e9af84465b7d55779a9b767aa27f8896a1c2da9e
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-10-21 11:58:39 +0100

    Created Tech Proficiency icons
    - Created list of icons to show personal tech proficiencies

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -6,5 +6,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor(){}
 
+  technologies: {iconURL: string, caption: string}[] = [
+    {
+      iconURL: '/assets/images/tech-icons/AngularJS.svg',
+      caption: 'AngularJS'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/Bootstrap.svg',
+      caption: 'Bootstrap'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/CSS3.svg',
+      caption: 'CSS3'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/Express.svg',
+      caption: 'Express'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/Git.svg',
+      caption: 'Git'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/HTML5.svg',
+      caption: 'HTML5'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/JavaScript.svg',
+      caption: 'JavaScript'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/Node.js.svg',
+      caption: 'Node.js'
+    },
+    {
+      iconURL: '/assets/images/tech-icons/React.svg',
+      caption: 'React'
+    },
+  ]
 }
