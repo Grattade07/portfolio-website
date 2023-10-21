bf144f6d64ebec9a2b1f2a2a2992e034f9364e60
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-10-22 00:33:11 +0100

    Navbar becomes dropdown in small screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AboutComponent } from './components/about/about.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ProjectsComponent } from './components/projects/projects.component';
-import { ContactPageComponent } from './components/contact-page/contact-page.component'
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
@@ -17,16 +17,17 @@ import { ContactPageComponent } from './components/contact-page/contact-page.com
     HeaderComponent,
     AboutComponent,
     ProjectsComponent,
-    ContactPageComponent
+    ContactPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    NgbDropdownModule,
     FontAwesomeModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
-import { faGlobe } from '@fortawesome/free-solid-svg-icons';
+import { faGlobe, faBars } from '@fortawesome/free-solid-svg-icons';
+
+import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
+  providers: [NgbDropdownConfig],
 })
-export class HeaderComponent {
-  faGlobe = faGlobe
+export class HeaderComponent implements OnInit {
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    console.log(this.innerWidth);
+    this.innerWidth = window.innerWidth;
+  }
+
+  constructor(config: NgbDropdownConfig) {
+    this.onResize();
+
+    config.placement = 'bottom-end';
+    config.autoClose = true;
+  }
+
+  faGlobe = faGlobe;
+  faHamMenu = faBars;
+
+  innerWidth: any;
+
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
 }
