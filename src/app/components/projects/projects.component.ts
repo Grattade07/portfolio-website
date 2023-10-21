7c054f83a3de9e5af3cb5e49cab3b23ec5158ba9
Grattade07 <joe.woodcock15@hotmail.co.uk>
2023-10-21 19:50:53 +0100

    Styling changes for Project Cards

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,15 +9,15 @@ export class ProjectsComponent {
 
   projects: {projectName: string, projectDescription:string, projectSourceURL: string, projectHostURL: string, previewImgPath: string}[] = [
     {
-      projectName: 'Web Project List',
-      projectDescription: 'Application built using React and Express that allows a user to interact with a custom API to create and manage a list of web applications',
-      projectSourceURL: 'https://github.com/Grattade07/Web-Project-List',
-      projectHostURL: 'https://web-project-list.onrender.com/',
-      previewImgPath: '/assets/project-preview-imgs/Web-Project-List-Preview.png'
+      projectName: 'Lab Inventory Management System',
+      projectDescription: 'A single-page web application that functions as an inventory management for a laboratory. Built using React and Bootstrap on the frontend, with Express and Mongoose communicating with MongoDB using a custom API, with middleware and JWTokens used to authenticate users.',
+      projectSourceURL: 'https://github.com/Grattade07/laboratory-inventory',
+      projectHostURL: 'https://laboratory-inventory.onrender.com/',
+      previewImgPath: '/assets/project-preview-imgs/Lab-Inventory-List-Preview.png'
     },
     {
       projectName: 'Web Project List',
-      projectDescription: 'Application built using React and Express that allows a user to interact with a custom API to create and manage a list of web applications',
+      projectDescription: 'Application built using React and Express that allows a user to interact with a custom API to create and manage a list of web applications.',
       projectSourceURL: 'https://github.com/Grattade07/Web-Project-List',
       projectHostURL: 'https://web-project-list.onrender.com/',
       previewImgPath: '/assets/project-preview-imgs/Web-Project-List-Preview.png'
